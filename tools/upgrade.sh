9dd5d16adf5a4fd15e2c8b62852ccdaedd50e36e
Paul des Garets <paul.des-garets@student.ecp.fr>
2015-04-21 02:37:03 +0200

    fix upgrade

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,4 +1,3 @@
-
 # Use colors, but only if connected to a terminal, and that terminal
 # supports them.
 tput=$(which tput)
@@ -20,30 +19,32 @@ else
   BOLD=""
   NORMAL=""
 fi
+function update {
+if git pull origin master && git pull --rebase --stat upstream master && git push origin master --force
+then
+        printf '%s' "$GREEN"
+        printf '%s\n' '         __                                     __   '
+        printf '%s\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+        printf '%s\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+        printf '%s\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+        printf '%s\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+        printf '%s\n' '                        /____/                       '
+        printf "${BLUE}%s\n" "Hooray! Oh My Zsh has been updated and/or is at the current version."
+        printf "${BLUE}${BOLD}%s${NORMAL}\n" "To keep up on the latest news and updates, follow us on twitter: https://twitter.com/ohmyzsh"
+        printf "${BLUE}${BOLD}%s${NORMAL}\n" "Get your Oh My Zsh swag at:  http://shop.planetargon.com/"
+else
+        printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
+fi
+}
 
 printf "${BLUE}%s${NORMAL}\n" "Upgrading Oh My Zsh"
 cd "$ZSH"
 if git remote show upstream
-then update
+        then update
 else
-git remote add upstream https://github.com/robbyrussell/oh-my-zsh.git
-git fetch upstream
-git checkout master
+        git remote add upstream https://github.com/robbyrussell/oh-my-zsh.git
+        git fetch upstream
+        git checkout master
+        update
 fi
-function update {
-if git pull origin master && git pull --rebase --stat upstream master
-then
-  printf '%s' "$GREEN"
-  printf '%s\n' '         __                                     __   '
-  printf '%s\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
-  printf '%s\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
-  printf '%s\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
-  printf '%s\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
-  printf '%s\n' '                        /____/                       '
-  printf "${BLUE}%s\n" "Hooray! Oh My Zsh has been updated and/or is at the current version."
-  printf "${BLUE}${BOLD}%s${NORMAL}\n" "To keep up on the latest news and updates, follow us on twitter: https://twitter.com/ohmyzsh"
-  printf "${BLUE}${BOLD}%s${NORMAL}\n" "Get your Oh My Zsh swag at:  http://shop.planetargon.com/"
-else
-  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
-fi
-}
+
